78463f6e5b4ba501e9b506bb5f8ffa062cc3a79d
devanshu121 <devanshumishra543@gmail.com>
2023-02-21 22:26:07 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This web app predicts the species of penguins as a function of their input parameters (bill length, bill width, flipper length, body mass, sex and island).',
+    author='Devanshu ',
+    license='MIT',
+)
